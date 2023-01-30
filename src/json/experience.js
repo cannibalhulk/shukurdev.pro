5a2a45f00b37a645c347f80c59c35d055d4d0d02
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-30 22:27:15 +0400

    create experinces array

---
+++ src/json/experience.js
@@ -0,0 +1,20 @@
+export const experinces = [
+    {
+        id:1,
+        company: 'Algoritmika bootcamp',
+        title: 'Project Manager',
+        beginMonth:'October',
+        endMonth:'October',
+        beginYear:2022,
+        endYear:2022,
+    },
+    {
+        id:2,
+        company: 'MonoFi',
+        title: 'Intern, Frontend Developer',
+        beginMonth:'January',
+        endMonth:'Present',
+        beginYear:2023,
+        endYear:'Present',
+    }
+]
