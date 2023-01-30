50e57255f193a59ea745c2d1413fb7ae5fdefa95
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-30 17:01:30 +0400

    backward some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,19 +13,20 @@ import tailwind from "@astrojs/tailwind";
   and leave it empty or use localhost URL. It won't break anything.
 */
 
-const SERVER_PORT = 3000;
+const SERVER_PORT = 4000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
 const LIVE_URL = "https://www.shukurdev.pro";
 // this is the astro command your npm script runs
-//NOTE: const SCRIPT = process.env.npm_lifecycle_script || "";
-//NOTE: const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = LIVE_URL;
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
 // When you're building your site in local or in CI, you could just set your URL manually
-//NOTE: if (isBuild) {
-//  BASE_URL = LIVE_URL;
-// }
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
   server: { port: SERVER_PORT },
   site: BASE_URL,
