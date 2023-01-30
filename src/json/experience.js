396e4d1cd70a34dfcd20b679b331dfb177e42cdd
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-31 01:01:43 +0400

    add `ul` array

--- src/json/experience.js
+++ src/json/experience.js
@@ -7,10 +7,20 @@ export const experinces = [
         endMonth:'October',
         beginYear:2022,
         endYear:2022,
+        description:"I worked as project manager in Algoritmika boot camp's HTML & CSS module project",
+        ul:[
+            "I build the website's about section.",
+            "I collaborated with my project mates via the Slack app, also tried to approach each ofthem individually as I can.",
+            "We performed well time management skill.",
+            "On final day we hosted our website in Netlify."
+        ]
+            
+        
     },
     {
         id:2,
         company: 'MonoFi',
+        linkedIn:"https://www.linkedin.com/company/manatpay/",
         title: 'Intern, Frontend Developer',
         beginMonth:'January',
         endMonth:'Present',
