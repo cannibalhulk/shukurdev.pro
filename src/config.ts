831766120f89441de63775c59e796b980dbaa533
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-30 13:59:08 +0400

    edit `SITE_TITLE` `SITE_DESCRIPTION`

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Shukur Huseynli";
+export const SITE_TITLE = "shukurdev.pro";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I mostly write about modern web development, coding and lifestyle";
+  "shukurdev.pro © 2023. Welcome to my blog! I mostly write about modern web development, coding and lifestyle";
 export const TWITTER_HANDLE = "@cannibal_dev";
 export const MY_NAME = "Shukur Huseynli";
 
