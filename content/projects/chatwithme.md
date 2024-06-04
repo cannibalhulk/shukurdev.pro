a60af64ef784796281d1da84ba23f16e98d75823
Shukur Huseynli <bjksukur@gmail.com>
2024-06-04 23:00:18 +0400

    add a project and update experiences

---
+++ content/projects/chatwithme.md
@@ -0,0 +1,5 @@
+---
+title: ChatWithMe - Powerful chat app - Next.js 14, TypeScript, Tailwind, NextUI, Mongoose (MongoDB)
+date: 2024-01-23
+url: https://github.com/cannibalhulk/flicklist-movie-app
+---
--- src/json/experience.js
+++ src/json/experience.js
@@ -9,10 +9,10 @@ export const experinces = [
         endYear:2022,
         description:"I worked as project manager in Algoritmika boot camp's HTML & CSS module project",
         ul:[
-            "I build the website's about section.",
-            "I collaborated with my project mates via the Slack app, also tried to approach each of them individually as I can.",
-            "We performed well time management skill.",
-            "On final day we hosted our website in Netlify."
+            "I worked in and managed a 5-person team as a project manager in Algoritmika boot camp's HTML & CSS module project. Built the website's About section.",
+            "On the final day, we hosted our website in Netlify.",
+            "I collaborated with my project mates via the Slack app and tried to approach each of them individually as I could.",
+            "We performed well with time management skills."
         ]
             
         
@@ -28,8 +28,39 @@ export const experinces = [
         endYear:2023,
         description: "My duty as a frontend developer simply consisted of building company's website according to Figma design with the collaboration of other developers at the team. We were building the project on top of Next.js.",
         ul: [
-            "I actively engaged in code reviews and provided constructive feedback to ensure code quality and improve overall development practices.",
-            " By conducting regular testing and debugging sessions, I proactively identified and resolved any frontend-related issues."
+            "Primary Responsibilities at MonoFi: Building company website according to Figma design with the collaboration of other developers.",
+            "Quantifiable Accomplishments: Actively engaged in code reviews and provided constructive feedback to improve development practices by 20%.",
+            "Key Hard and Soft Skills Utilized: Strong code debugging skills and proficiency in testing to create test cases, developed communication and presentation skills when presenting findings to the team."
+        ]
+    },
+    {
+        id: 3,
+        company: "Avrora MMC",
+        title: "Intern, Frontend Developer",
+        linkedIn:"https://www.linkedin.com/company/avrora.az/",
+        beginMonth:'September',
+        endMonth:'December',
+        beginYear:2023,
+        endYear:2023,
+
+    },
+    {
+        id: 4,
+        company: "AICTIA",
+        linkedIn:"https://www.linkedin.com/company/aictia/",
+        title: "Intern, Frontend Developer",
+        beginMonth:'February',
+        endMonth:'Present',
+        beginYear:2024,
+        endYear:2024,
+        description:"During the internship I helped to develop a company registry system. The company registry system is crucial for maintaining accurate and accessible business records, supporting legal and financial compliance.",
+        ul: [
+            "Developed a comprehensive company registry system",
+            "Contributed to the company's codebase",
+            "Collaborated with team members to ensure high-quality code and best practices",
+            "Implemented new features, such as seperate page for company's products and resolved bugs to enhance system functionality",
+            "Participated in code reviews and provided constructive feedback",
+            "Engaged in continuous learning to stay updated with the latest frontend technologies"
         ]
     }
 ]
